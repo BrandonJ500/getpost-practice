92446d308b987a401848484b586107f5a7311e2b
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-03-30 15:30:13 -0700

    added urlcoded to app

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
 
+
+app.use(express.urlencoded({extended:true}))
+
 app.get("/tacos", (req,res)=>{
     res.send("get /tacos responds");
 })
 
 app.post("/tacos", (req,res)=>{
+    console.log(req.body)
     res.send("post /tacos responds")
 })
 app.listen(3000, ()=> {
