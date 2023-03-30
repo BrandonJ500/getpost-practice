25b0deb0e0836efecb2bc9058236a5fca1a63233
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-03-30 16:50:59 -0700

    added ejs, view engine and path

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 
 app.use(express.urlencoded({extended:true}))
-
+app.use(express.json())
+app.set("views", path.join(__dirname,"views"))
+app.set("view engine", "ejs")
 app.get("/tacos", (req,res)=>{
     res.send("get /tacos responds");
 })
+
+const comments = [
+    {
+        username: "todd",
+        comment: "lol Im todd"
+    },
+    {
+        username: "Skylar",
+        comment: "I like to go birdwatching with my dog"
+    },
+    {
+        username: "Sk8erBoi",
+        comment: "Plz delete your account, todd"
+    },
+    {
+        username: "onlysayswoof",
+        comment: "woof woof woof"
+    }
+]
 
 app.post("/tacos", (req,res)=>{
     console.log(req.body)
@@ -15,3 +36,5 @@ app.post("/tacos", (req,res)=>{
 app.listen(3000, ()=> {
     console.log("port 3000")
 })
+
+
