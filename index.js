8dc43edec418120f2ed4ef4ae7655045a03f8b8c
Brandon Joseph <Brandonjoseph805@gmail.com>
2023-03-30 20:38:04 -0700

    add uuid to give unique ids to comments. add link to go from comments to input form

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const {v4: uuid} = require("uuid")
 
-app.use(express.urlencoded({extended:true}))
+
+
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.set("views", path.join(__dirname,"views"))
+app.set("views", path.join(__dirname, "views"))
 app.set("view engine", "ejs")
-app.get("/tacos", (req,res)=>{
+app.get("/tacos", (req, res) => {
     res.send("get /tacos responds");
 })
 
 const comments = [
     {
+        id: uuid(),
         username: "todd",
         comment: "lol Im todd"
     },
     {
+        id: uuid(),
         username: "Skylar",
         comment: "I like to go birdwatching with my dog"
     },
     {
+        id: uuid(),
         username: "Sk8erBoi",
         comment: "Plz delete your account, todd"
     },
     {
+        id: uuid(),
         username: "onlysayswoof",
         comment: "woof woof woof"
     }
 ]
 
-app.get("/comments/new", (req,res)=>{
+app.get("/comments/new", (req, res) => {
     res.render("comments/new")
 })
 
-app.get("/comments", (req,res)=> {
-    res.render("comments/index",{comments})
+app.get("/comments", (req, res) => {
+    res.render("comments/index", { comments })
 })
-app.post("/tacos", (req,res)=>{
+app.post("/tacos", (req, res) => {
     console.log(req.body)
     res.send("post /tacos responds")
 })
 
-app.post("/comments", (req,res)=> {
-   console.log(req.body)
-   const {username, comment} = req.body;
-   comments.push({username, comment})
-    res.send("It worked")
+app.post("/comments", (req, res) => {
+    console.log(req.body)
+    const { username, comment } = req.body;
+    comments.push({ username, comment, id:uuid() })
+    res.redirect("/comments")
 })
-app.listen(3000, ()=> {
+
+app.get("/comments/:id", (req,res)=>{
+   const {id}= req.params;
+  const comment = comments.find((c=> c.id === id));
+  res.render("comments/show",{comment});
+})
+
+app.listen(3000, () => {
     console.log("port 3000")
 })
 
